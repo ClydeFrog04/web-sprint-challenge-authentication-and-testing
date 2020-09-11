31e748573797b56402c6301551162fb56d07ff80
ClydeFrog04 <egan.randal@gmail.com>
2020-09-11 17:19:53 -0600

    chore(authenticate-middleware.ts): restrict middleware
    Created restrict middleware function

--- auth/authenticate-middleware.ts
+++ auth/authenticate-middleware.ts
@@ -1,9 +1,24 @@
-/* 
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 import {Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
 
-export const restrict = (req:Request, res:Response, next:NextFunction) => {
-  res.status(401).json({ you: 'shall not pass!' });
+export const restrict = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authError = {message: "Invalid credentials provided"};
+
+        const token = req.headers.authorization ?? req.cookies.token;//checking for both depending on what the client provides
+
+        if (!token) return res.status(401).json(authError);
+
+        jwt.verify(token, process.env.JWT_SECRET!, (err:any, decoded:any) =>{
+            if(err) return res.status(401).json(authError);
+            next();
+        });
+    } catch (e) {
+        console.log(e.stack);
+    }
+
 };
